14d621f9da03d30f09278e033813c16d5ccf5677
NGUYEN TRONG TUNG <nguyentrongtung11101999@gmail.com>
2020-06-28 11:42:52 +0700

    Adding data

--- source/backend/AI_source/AI_Text_Repo/coco_api_party/COCOAPIManager.py
+++ source/backend/AI_source/AI_Text_Repo/coco_api_party/COCOAPIManager.py
@@ -1,13 +1,14 @@
 ### This class is used for talking to show_and_tell repository
 from pycocotools.coco import COCO
 import numpy as np
+
 class COCOAPIManager():
 
     def __init__(self, request):
         self.request= request
     
     def init_COCO_model(self):
-        dataDir = '..'
+        dataDir = '/home/nttung/CS422-Project/data_COCO'
         dataType = 'val2017'
         annFile='{}/annotations/instances_{}.json'.format(dataDir,dataType)
         return COCO(annFile)
@@ -16,28 +17,40 @@ class COCOAPIManager():
         # returned list of keyword in text
         return query_text.split(" ")
 
+    def filter_most_relevant(self, imgId, limit_size):
+        return imgId
+
     def retrieve(self, request):
-        # # get coco instance
-        # coco = self.init_COCO_model()
+        # get coco instance
+        coco = self.init_COCO_model()
 
 
-        # # extract field
-        # limit_size = request["limit_size"]
-        # text = request["text"]
-        # norm_q_list = self.normalize_query_list(text)
+        # extract field
+        limit_size = request["limit_size"]
+        text = request["text"]
+        norm_q_list = self.normalize_query_list(text)
 
-        # catIds = coco.getCatIds(catNms=norm_q_list)
-        # imgIds = coco.getImgIds(catIds=catIds)
-        # imgIds = coco.getImgIds(imgIds = [limit_size])
-        # img = coco.loadImgs(imgIds[np.random.randint(0,len(imgIds))])[0]
+        catIds = coco.getCatIds(catNms=norm_q_list)
+        imgIds = coco.getImgIds(catIds=catIds)
+        print(imgIds)
+        limit_size = min(limit_size, len(imgIds))
+        
+        imgIds = self.filter_most_relevant(imgIds, limit_size)
 
-        ###testing
         res = {}
         res["list_img"] = []
-        res["list_img"].append({"name": "a","content":"img_content_a"})
-        res["list_img"].append({"name": "b","content":"img_content_b"})
- 
+        for each_id in imgIds:
+            print(each_id)
+            res["list_img"].append(coco.loadImgs(each_id)[0])
+
         return res
+        # ###testing
+        # res = {}
+        # res["list_img"] = []
+        # res["list_img"].append({"name": "a","content":"img_content_a"})
+        # res["list_img"].append({"name": "b","content":"img_content_b"})
+ 
+        # return res
 
     # def refine(self, request):
 
