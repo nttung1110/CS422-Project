8afce1e0484605c8b7e11b39e0d37c6db71a06ee
glaciel <42582262+glaciel@users.noreply.github.com>
2020-06-30 16:30:29 +0700

    Add Sketch API Service

--- source/backend/AI_source/AI_Services_Manger.py
+++ source/backend/AI_source/AI_Services_Manger.py
@@ -1,5 +1,6 @@
 ## serve all services involving with AI, produce services and act as factory
 from AI_source.AI_Text_Repo.AI_Text_Services import AI_Text_Services
+from AI_source.AI_Sketch_Repo.AI_Sketch_Services import AI_Sketch_Services
 class AI_Services_Manager():
     def __init__(self):
         #gather all involving resources
@@ -9,7 +10,7 @@ class AI_Services_Manager():
 
     def add_default_services(self):
         self.services["AI_Text_Services"] = AI_Text_Services()
-        # self.services["AI_Sketch_Services"] = AI_Sketch_Services
+        self.services["AI_Sketch_Services"] = AI_Sketch_Services()
 
     def add_AI_Services(self, service_x_name, AI_X_Services):
         self.services[service_x_name] = AI_X_Services
