67c806e9fa0d484160ff8d8527796a31358f61c7
glaciel <42582262+glaciel@users.noreply.github.com>
2020-07-01 10:01:27 +0700

    Add Image Captioning API

--- source/backend/AI_source/AI_Services_Manger.py
+++ source/backend/AI_source/AI_Services_Manger.py
@@ -1,6 +1,8 @@
 ## serve all services involving with AI, produce services and act as factory
 from AI_source.AI_Text_Repo.AI_Text_Services import AI_Text_Services
 from AI_source.AI_Sketch_Repo.AI_Sketch_Services import AI_Sketch_Services
+from AI_source.AI_Image_Captioning_Repo.AI_Image_Captioning_Services import AI_ImgCaption_Services
+
 class AI_Services_Manager():
     def __init__(self):
         #gather all involving resources
@@ -11,6 +13,7 @@ class AI_Services_Manager():
     def add_default_services(self):
         self.services["AI_Text_Services"] = AI_Text_Services()
         self.services["AI_Sketch_Services"] = AI_Sketch_Services()
+        self.services["AI_Image_Captioning_Services"] = AI_ImgCaption_Services()
 
     def add_AI_Services(self, service_x_name, AI_X_Services):
         self.services[service_x_name] = AI_X_Services
